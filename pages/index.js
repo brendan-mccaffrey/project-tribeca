840134643d13e206b8632d4daee15831c945548a
brendan-mccaffrey <brendanchristophermccaffrey@gmail.com>
2023-10-20 12:53:03 -0400

    remove experimental features

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { Footer, Navbar } from '../components';
-import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+import { About, Explore, Hero } from '../sections';
+// import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 const Home = () => (
   <div className="bg-primary-black overflow-hidden">
@@ -10,7 +11,7 @@ const Home = () => (
       <div className="gradient-03 z-0" />
       <Explore />
     </div>
-    <div className="relative">
+    {/* <div className="relative">
       <GetStarted />
       <div className="gradient-04 z-0" />
       <WhatsNew />
@@ -20,7 +21,7 @@ const Home = () => (
       <Insights />
       <div className="gradient-04 z-0" />
       <Feedback />
-    </div>
+    </div> */}
     <Footer />
   </div>
 );
